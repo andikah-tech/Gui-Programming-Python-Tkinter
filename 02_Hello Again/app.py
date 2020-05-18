cee30a7143154b3d5f9e57c9e3f625a6d68bfbbb
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-05-17 19:19:24 -0700

    modified file app.py in part 02_hello again

--- 02_Hello Again/app.py
+++ 02_Hello Again/app.py
@@ -13,24 +13,27 @@ class App(Frame):
 
 	# method
 	def widgets(self):
+		# set label
+		self.label = Label(self, text="Hello I'm GUI *_*").grid(columnspan=2)
 		# click button
 		self.hi = Button(self)
 		self.hi["text"] = "Click Here!"
 		self.hi["command"] = self.ok
-		self.hi.grid(row=0, column=0)
+		self.hi.grid(row=1, column=0)
 
 		# quit button
 		self.quit = Button(self)
 		self.quit["text"] = "QUIT"
 		self.quit["command"] = self.master.destroy
 		self.quit["fg"] = "red"
-		self.quit.grid(row=0, column=1)
+		self.quit.grid(row=1, column=1)
 
 	def ok(self):
 		self.click = Label(self, text="Button Onclick!", fg="blue").grid(columnspan=2)
 
 
 root = Tk()
+root.title("Hello Again")
 # object
 myapp = App(master=root)
 myapp.mainloop()
